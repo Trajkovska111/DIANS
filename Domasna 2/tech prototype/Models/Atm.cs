eeec301caa99224997d16a39533dbdbb8e93b0a4
fetajif <fetajif@gmail.com>
2020-12-06 23:54:44 +0100

    finalno

---
+++ Domasna 2/tech prototype/Models/Atm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AtmLocator.Models
+{
+    public class Atm
+    {
+        public int ID { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+        [Required]
+        public double Latitude { get; set; }
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string OpenHours { get; set; }
+        public Boolean Wheelchair { get; set; }
+        public Boolean Drive_Through { get; set; }
+
+    }
+}
